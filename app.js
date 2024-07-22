25a1ac4a7a8badfd78d67b4708ba83e27541f6d3
Abdi Setiawan <bedikadiryt@gmail.com>
2024-07-22 20:42:22 +0700

    update

--- app.js
+++ app.js
@@ -15,15 +15,12 @@ const sellerRouter = require("./routes/seller");
 const app = express();
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
 app.use(bodyParser.json());
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/admin", adminRouter);
 app.use("/consumer", consumerRouter);
@@ -38,6 +35,11 @@ app.get("/", (req, res) => {
   res.send("Selamat datang di Aplikasi Jual Beli Sepatu by Novia Nirwana");
 });
 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
@@ -49,9 +51,11 @@ app.use(function (err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+  // send the error response
+  res.status(err.status || 500).send({
+    message: err.message,
+    error: req.app.get("env") === "development" ? err : {},
+  });
 });
 
 module.exports = app;
